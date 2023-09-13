32947e07b954076c17564489ff32edda468095f6
gomaa <gogochemist35@gmail.com>
2023-09-13 19:07:04 +0300

    another code

--- problem11 palindrom number/problem11 palindrom number.cpp
+++ problem11 palindrom number/problem11 palindrom number.cpp
@@ -2,8 +2,56 @@
 #include <string>
 using namespace std;
 
+int readPositiveNumber(string msg)
+{
+	int num=0;
 
-int readNumber(string msg)
+	do
+	{
+		cout << msg << endl;
+		cin >> num;
+	} while (num<=0);
+
+	return num;
+}
+
+int reverseNumberDigits(int num)
+{
+	int reversedNumber = 0;
+
+	while (num > 0)
+	{
+		int reminder = num % 10;
+		num = num / 10;
+
+		reversedNumber = reversedNumber * 10 + reminder;
+	}
+	return reversedNumber;
+}
+
+bool checkPalindromeNumber(int num)
+{
+	int reversedNumber = reverseNumberDigits(num);
+	return num == reversedNumber;
+}
+
+void printCheckPalindromeNumber(bool res)
+{
+	if (res)
+		cout << " Yes it is a Palindrome number\n\n ";
+	else
+		cout << " No it is not a Palindrome number\n\n ";
+
+}
+
+int main()
+{
+	printCheckPalindromeNumber(checkPalindromeNumber(readPositiveNumber("please enter a number?")));
+
+	return main();
+}
+
+/*int readNumber(string msg)
 {
 	int num;
 	cout << msg << endl;
@@ -47,11 +95,13 @@ void checkPalindromeNumber(int num)
 	{
 		cout << "No, it is Not a Palindrome number.\n\n";
 	}
-}
 
+
+}
 int main()
 {
 	checkPalindromeNumber(readNumber("enter Number?"));
 
 	return main();
 }
+*/
